62391bf58701f5a8daf5c168f035cd3c1828f8ff
Torres Silva Gabriel <gabriel.torres@student.vinci.be>
2022-11-21 12:09:51 +0100

    :recycle: Fixed webpack.config file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
-import './img/logo.png';
 
 // import Navbar from './Components/Navbar/Navbar';
 import Router from './Components/Router/Router';
