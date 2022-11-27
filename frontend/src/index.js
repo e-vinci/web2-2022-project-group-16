95ac476114d54e380f968ccb230b23cce7269fdb
Torres Silva Gabriel <gabriel.torres@student.vinci.be>
2022-11-27 18:05:19 +0100

    :sparkles: Added rulesModal component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
 
+import RulesModal from './Components/Modal/RulesModal';
 // import Navbar from './Components/Navbar/Navbar';
 import Router from './Components/Router/Router';
 
 // Navbar();
 
 Router();
+RulesModal();
