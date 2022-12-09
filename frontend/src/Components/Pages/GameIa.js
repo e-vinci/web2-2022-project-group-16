50737e06fdfc1ff38cfe75947e518dca4155b56b
juliendec <juliendecoen2308@gmail.com>
2022-12-09 09:45:01 +0100

    dynamic display cards

--- frontend/src/Components/Pages/GameIa.js
+++ frontend/src/Components/Pages/GameIa.js
@@ -2,78 +2,66 @@ import menuBtn from '../../img/menu.png';
 import backBtn from '../../img/back.png';
 import GetButtons from '../Router/GetButtons';
 import rulesBtn from '../../img/rules.png';
-import GetCard from '../../utils/front-script';
+import { GetCard, run } from '../../utils/front-script';
 
-const translateCard = (num) => {
 
-  const arrayTranslate = ["card","ace","two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"];
 
-  return arrayTranslate[num];
+const CreditsPage = () => {
+  // eslint-disable-next-line no-use-before-define
+  mainHeader();
+  // eslint-disable-next-line no-use-before-define
+  createBody();
+  GetButtons();
+  run();
+  GetCard();
+  // eslint-disable-next-line no-use-before-define
+  mainFooter();
+};
+
+const createBody = () => {
+  // create a new div element 
+	const game = document.createElement("div");
+	// eslint-disable-next-line camelcase
+	const carduser = document.createElement("div"); 
+	const cardback = document.createElement("div"); 
+	const cardvisible = document.createElement("div"); 
+  const cardhand = document.createElement("div"); 
+
+  // assigning class name to the new div
+  game.id = "game";
+  carduser.className = "card-user";
+  cardback.className = "card-back";
+  cardvisible.className = "card-back";
+  cardhand.className = "card-hand";
+  cardhand.id = "card-hand";
+
+  const main = document.querySelector('main');
+  main.appendChild(game);
+  game.appendChild(carduser);
+  carduser.appendChild(cardhand)
+  game.appendChild(cardback);
+  game.appendChild(cardvisible);
 
 }
 
-const CreditsPage = () => {
+const mainHeader = () => {
+    const main = document.querySelector('main');
+
+    main.innerHTML = `
+    <header class="px-3 pt-3 d-flex justify-content-between align-items-center">
+    <button data-uri="/" class="navigation-btn"><img id="rules-btn" class="ms-2" src="${backBtn}" alt="rules button"></button>
+    <a href="#"><img id="rules-btn" class="ms-2" src="${menuBtn}" alt="rules button"></a>
+    </header>
+    `;
+}
+
+const mainFooter = () => {
   const main = document.querySelector('main');
-  main.innerHTML = `
-  <header class="px-3 pt-3 d-flex justify-content-between align-items-center">
-  <button data-uri="/" class="navigation-btn"><img id="rules-btn" class="ms-2" src="${backBtn}" alt="rules button"></button>
-  <a href="#"><img id="rules-btn" class="ms-2" src="${menuBtn}" alt="rules button"></a>
-  </header>
-  <div id="game" class="">
 
-    <div class="card-user">
-      <div class="card-hand">
-        <div class="card-content three">
-          <div class="cards spades ${translateCard(3)}" data-number="three" data-type="spades" ></div>
-        </div>
-        <div class="card-content nine">
-          <div class="cards clubs nine"></div>
-        </div>
-        <div class="card-content two">
-          <div class="cards diamonds two"></div>
-        </div>
-        <div class="card-content king">
-          <div class="cards hearts king"></div>
-        </div>
-        <div class="card-content jack">
-          <div class="cards clubs jack"></div>
-        </div>
-        <div class="card-content three">
-          <div class="cards clubs three"></div>
-        </div>
-      </div>
+  main.innerHTML += `
+  <footer class="px-3 pb-3 d-flex justify-content-end align-items-center"><div id="rules"><a href="#">rules</a><img id="rules-btn" class="ms-2" src="${rulesBtn}" alt="rules button"></div></footer>
+  `;
+}
 
-      <div class="card-back">
-        <div class="card-content">
-          <div class="cards"></div>
-        </div>
-        <div class="card-content">
-          <div class="cards"></div>
-        </div>
-        <div class="card-content">
-          <div class="cards"></div>
-        </div>
-      </div>
-
-      <div class="card-visible">
-        <div class="card-content king">
-          <div class="cards hearts king"></div>
-        </div>
-        <div class="card-content jack">
-          <div class="cards clubs jack"></div>
-        </div>
-        <div class="card-content three">
-          <div class="cards clubs three"></div>
-        </div>
-      </div>
-
-    </div>
-
-  </div>
-  <footer class="px-3 pb-3 d-flex justify-content-end align-items-center"><div id="rules"><a href="#">rules</a><img id="rules-btn" class="ms-2" src="${rulesBtn}" alt="rules button"></div></footer>`;
-
-  GetButtons();
-  GetCard();
-};
 
 export default CreditsPage;
--- frontend/src/utils/front-script.js
+++ frontend/src/utils/front-script.js
@@ -1,6 +1,10 @@
+const orianterObjet = require('../../../api/routes/orianterObjet');
 
-const GetCard = () => {
-    const card = document.querySelectorAll('.cards');
+const danish = new orianterObjet.Danish(2);
+
+
+function GetCard() {
+    const card = document.querySelectorAll('div.cards');
 
     Array.from(card).forEach((elem) => {
         elem.addEventListener('click', () => {
@@ -10,5 +14,55 @@ const GetCard = () => {
         });
     })
 }
+
+function translateCard(num) {
+
+    const arrayTranslate = ["card","ace","two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"];
+  
+    return arrayTranslate[num];
+  
+}
+
+function renderCards() {
+    const divCardPlayer = document.querySelector("#card-hand");
+    
+    danish.cardsPlayable(danish.discardPile[0]);
+
+    const playerHand = danish.tablePlayerGame[0].tableHands;
+    console.log("main ");
+    console.log(playerHand)
+    console.log("main special card ");
+    console.log(playerHand[0].value)
+
+
+    // eslint-disable-next-line no-plusplus
+    for(let i=0; i < playerHand.length; i++){
+        const cardcontent = document.createElement("div"); 
+        const cards = document.createElement("div"); 
+
+        cardcontent.className = "card-content";
+        cardcontent.className += " ";
+        cardcontent.className += translateCard(playerHand[i].value);
+
+        cards.className = "cards";
+        cards.className += " ";
+        cards.className += playerHand[i].color;
+        cards.className += " ";
+        cards.className += translateCard(playerHand[i].value);
+        cards.dataset.number = translateCard(playerHand[i].value);
+        cards.dataset.type = playerHand[i].color;
+
+        divCardPlayer.appendChild(cardcontent);
+        cardcontent.appendChild(cards);
+    }
+
+}
+
+
+function run() {
+    renderCards();
+    danish.nextPlayer();
+};
+
  
-export default GetCard;
+module.exports = { GetCard, run }
