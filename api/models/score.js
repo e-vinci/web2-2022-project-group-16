d2c78ff2e295152d67c1571a57d112c2059e35f9
Quisquater Martin <martin.quisquater@student.vinci.be>
2022-12-16 09:29:47 +0100

    API SCORE

--- api/models/score.js
+++ api/models/score.js
@@ -5,56 +5,72 @@ const jsonDbPath = path.join(__dirname, '/../data/users.json');
 
 
 function win(player){
-    console.log("player")
-    console.log(player)
-    const user = parse(jsonDbPath);
-    const playerFound = findAUser(player)
-    console.log(`ceci est le player : ${playerFound}`)
-    playerFound.scoreWin += 1;
-
-
-    //  serialize(jsonDbPath,nombre);
-    //  const updatedPlayer= { ...playerFound,...nombre };
-  
-    // playerFound = updatedPlayer;
-  
-    // serialize(jsonDbPath, updatedPlayer);
-    // jsonDbPath.replace
-
-    return playerFound; 
+    const playerFoundWin = findUserUpdateWin(player);
+    return playerFoundWin; 
 }
 
 function lose(player){
-    const game = findAUser(player)
-    game.scoreLose += 1;
-    return game;
+    const playerFoundLose = findUserUpdateLose(player);
+    return playerFoundLose;
 }
-
 function numberGamePlayer(player){
-    const game = findAUser(player)
-    let numberGame = 0;
-    numberGame =  game.scoreWin + game.scoreLose;
-    return numberGame;
+    const userFound = findAUser(player);
+    if (!userFound) return undefined;
+    const total = userFound.scoreWin + userFound .scoreLose;
+    return total;
 }
 
-// function updatePlayer(id, winGame) {
-//     const idNumber = parseInt(id, 25);
-//     const PLAYER = parse(jsonDbPath);
-//     const foundPlayer = PLAYER.findIndex((playFoundThis) => playFoundThis.id === idNumber);
-//     if (foundPlayer < 0) return undefined;
-  
-//     const updatedPlayer= { ...PLAYER[foundPlayer], ...winGame };
-  
-//     PLAYER[foundPlayer] = updatePlayer;
-  
-//     serialize(jsonDbPath, PLAYER);
-  
-//     return updatePlayer;
-//   }
+function sortPlayer(){
+    const allPlayer = parse(jsonDbPath);
+    const tableSortPlayer = allPlayer.slice();
+    tableSortPlayer.sort((a,b) => {
+        // Calcule la somme des propriétés value1 et value2 pour chaque objet
+        const totA = a.scoreWin + b.scoreLose;
+        const sumA = a.scoreWin * totA;
+        const totb = a.scoreWin + b.scoreLose;
+        const sumB = b.scoreWin * totb;
+
+        // Si la somme de a est inférieure à celle de b, a doit être classé apres b
+        if (sumA < sumB) {
+            return 1;
+        }
+        // Si la somme de a est inférieure à celle de b, a doit être classé avant b
+        if (sumA > sumB) {
+            return -1;
+        }
+        // Si les sommes sont égales, a et b restent à leur position actuelle dans le tableau
+        return 0;
+        });
+            return tableSortPlayer;
+
+}
+
+
+function findUserUpdateWin(username){
+    const allUser = parse(jsonDbPath);
+    const indexOfUser = allUser.findIndex((user) => user.username === username);
+    if (indexOfUser < 0) return undefined;
+    allUser[indexOfUser].scoreWin += 1 // change la valeur de +1
+    serialize(jsonDbPath, allUser)
+    return allUser[indexOfUser]
+}
+
+function findUserUpdateLose(username){
+    const allUser = parse(jsonDbPath);
+    const indexOfUser = allUser.findIndex((user) => user.username === username);
+    if (indexOfUser < 0) return undefined;
+    allUser[indexOfUser].scoreLose += 1 // change la valeur de +1
+    serialize(jsonDbPath, allUser)
+    return allUser[indexOfUser]
+}
+
+
+
 
 module.exports = {
     win,
     lose,
     numberGamePlayer,
+    sortPlayer,
     
   }
