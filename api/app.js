d91a76ecb22b822467fb6417641af2fadea0717f
Quisquater Martin <martin.quisquater@student.vinci.be>
2022-12-18 17:12:18 +0100

    cors

--- api/app.js
+++ api/app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'https://crapsdorian.github.io',
+};
+
 const usersRouter = require('./routes/users');
 const authsRouter = require('./routes/auths');
 const scoreRouter = require('./routes/score')
@@ -13,8 +19,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/users', usersRouter);
-app.use('/auths', authsRouter);
-app.use('/score', scoreRouter)
+app.use('/users',cors(corsOptions), usersRouter);
+app.use('/auths',cors(corsOptions), authsRouter);
+app.use('/score',cors(corsOptions), scoreRouter)
 
 module.exports = app;
